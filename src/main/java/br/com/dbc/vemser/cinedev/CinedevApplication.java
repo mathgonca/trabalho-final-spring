7317945c639b28441c5acf36109664e9d49babe9
Matheus Goncalves <matheus.goncalves@dbccompany.com.br>
2022-10-20 17:00:38 -0300

    Commit inicial

---
+++ src/main/java/br/com/dbc/vemser/cinedev/CinedevApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.cinedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinedevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinedevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbc/vemser/cinedev/CinedevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.cinedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinedevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
