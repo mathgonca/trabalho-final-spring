46c1c2c9b67d26c962617ec9cfaf0be5c87b275b
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-10-28 02:37:13 -0300

    Interface Ingresso e controller

--- src/main/java/br/com/dbc/vemser/cinedev/controller/IngressoController.java
+++ src/main/java/br/com/dbc/vemser/cinedev/controller/IngressoController.java
@@ -1,2 +1,63 @@
-package br.com.dbc.vemser.cinedev.controller;public class IngressoController {
+package br.com.dbc.vemser.cinedev.controller;
+
+import br.com.dbc.vemser.cinedev.controller.documentInterface.OperationControllerIngresso;
+import br.com.dbc.vemser.cinedev.dto.IngressoCompradoDTO;
+import br.com.dbc.vemser.cinedev.dto.IngressoCreateDTO;
+import br.com.dbc.vemser.cinedev.dto.IngressoDTO;
+import br.com.dbc.vemser.cinedev.exception.BancoDeDadosException;
+import br.com.dbc.vemser.cinedev.exception.RegraDeNegocioException;
+import br.com.dbc.vemser.cinedev.service.IngressoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ingresso")
+
+public class IngressoController implements OperationControllerIngresso {
+
+    private final IngressoService ingressoService;
+
+    @Override
+    @GetMapping
+    public List<IngressoCompradoDTO> listarIngressosComprados(Integer id) throws RegraDeNegocioException, BancoDeDadosException {
+        return ingressoService.listarIngressosComprados(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<IngressoDTO> listarIngressos() throws RegraDeNegocioException, BancoDeDadosException {
+        return ingressoService.listarIngressos();
+    }
+
+    @Override
+    @PostMapping
+    public ResponseEntity<IngressoDTO> createIngresso(@Valid @RequestBody IngressoCreateDTO ingresso) throws RegraDeNegocioException,
+            RegraDeNegocioException, BancoDeDadosException {
+        return new ResponseEntity<>(ingressoService.createIngresso(ingresso), HttpStatus.OK);
+    }
+
+    @Override
+    @PutMapping("/{idIngresso}")
+    public ResponseEntity<IngressoDTO> updateIngresso(@PathVariable("idIngresso") Integer id,
+                                                      @Valid @RequestBody IngressoCreateDTO ingresso) throws RegraDeNegocioException, BancoDeDadosException {
+        return new ResponseEntity<>(ingressoService.updateIngresso(id, ingresso), HttpStatus.OK);
+    }
+
+    @Override
+    @DeleteMapping("/{idIngresso}")
+    public ResponseEntity<Void> removeIngresso(@PathVariable("idIngresso") Integer id) throws RegraDeNegocioException, BancoDeDadosException {
+        ingressoService.removeIngresso(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
+
+
---
+++ src/main/java/br/com/dbc/vemser/cinedev/controller/documentInterface/OperationControllerIngresso.java
@@ -0,0 +1,54 @@
+package br.com.dbc.vemser.cinedev.controller.documentInterface;
+
+import br.com.dbc.vemser.cinedev.dto.*;
+import br.com.dbc.vemser.cinedev.exception.BancoDeDadosException;
+import br.com.dbc.vemser.cinedev.exception.RegraDeNegocioException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public interface OperationControllerIngresso {
+
+    @Operation(summary = "Listagem de Ingressos comprados ", description = "Lista os Ingressos comprados referentes a busca do banco")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Retorna a lista de Ingressos comprados de acordo com a pesquisa"),
+            @ApiResponse(responseCode = "403", description = "A algo de errado com as inserções de sua pesquisa"),
+            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")})
+    @GetMapping
+    public List<IngressoCompradoDTO> listarIngressosComprados(Integer id) throws RegraDeNegocioException, BancoDeDadosException;
+
+    @Operation(summary = "Lista os Ingressos ", description = "Lista os Ingressos referentes a busca do banco")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Retorna a lista de Ingressos de acordo com a pesquisa"),
+            @ApiResponse(responseCode = "403", description = "A algo de errado com as inserções de sua pesquisa"),
+            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")})
+    @GetMapping
+    public List<IngressoDTO> listarIngressos() throws RegraDeNegocioException, BancoDeDadosException;
+
+    @Operation(summary = "Cadastro.", description = "Cadastramento de dados de usuários")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Cadastro realizado com Sucesso!"),
+            @ApiResponse(responseCode = "403", description = "Erro na inserção de dados!"),
+            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")})
+    @PostMapping
+    public ResponseEntity<IngressoDTO> createIngresso(@Valid @RequestBody IngressoCreateDTO ingresso) throws RegraDeNegocioException,
+            RegraDeNegocioException, BancoDeDadosException;
+
+    @Operation(summary = "Edição e Atualização de Ingressos", description = "Edição e alteração de dados do Ingresso a partir da referencia de pesquisa 'ID'!")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Atualização de Ingresso realizada!"),
+            @ApiResponse(responseCode = "403", description = "Erro na inserção de dados!"),
+            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")})
+    @PutMapping("/{idIngresso}")
+    public ResponseEntity<IngressoDTO> updateIngresso(@PathVariable("idPessoa") Integer id,
+                                                      @Valid @RequestBody IngressoCreateDTO ingressoCreateDTO) throws RegraDeNegocioException, BancoDeDadosException;
+
+    @Operation(summary = "Remove Um Ingresso ", description = "Remoção do Ingresso  partir da 'idIngresso'!")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Remoção do Ingresso realizada!"),
+            @ApiResponse(responseCode = "403", description = "Erro na inserção de dados para busca!"),
+            @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")})
+    @DeleteMapping("/{idIngresso}") // localhost:8080/ingresso/10
+    public ResponseEntity<Void> removeIngresso(@PathVariable("idPessoa") Integer id) throws RegraDeNegocioException, BancoDeDadosException;
+
+}
