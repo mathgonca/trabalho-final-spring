bf37a0ae7d25492b82d26da8f0f9258913b707bc
JONAS DE LARA <jonasdelara_@Hotmail.com>
2022-10-29 02:46:45 -0300

    schema ingresso

--- src/main/java/br/com/dbc/vemser/cinedev/dto/IngressoCompradoDTO.java
+++ src/main/java/br/com/dbc/vemser/cinedev/dto/IngressoCompradoDTO.java
@@ -1,14 +1,33 @@
 package br.com.dbc.vemser.cinedev.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+
 @Data
 public class IngressoCompradoDTO {
+    @NotNull
+    @NotEmpty
+    @Schema(description = "ID do ingresso comprado ", example = "1")
     private int idIngressoComprado;
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Nome do cliente ", example = "Pablo Horacio Guiñazú")
     private String nomeCliente;
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Filme escolhido", example = "Adão Negro")
     private String nomeFilme;
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Cinema escolhido ", example = "GNC Iguatemi', 'RS', 'Porto Alegre'")
     private String nomeCinema;
+    @NotNull
+    @NotEmpty
+    @Schema(description = " horario e a data escolhido ", example = "'21/10/2022 21:30")
     private LocalDateTime DataHora;
 
     @Override
--- src/main/java/br/com/dbc/vemser/cinedev/dto/IngressoCreateDTO.java
+++ src/main/java/br/com/dbc/vemser/cinedev/dto/IngressoCreateDTO.java
@@ -1,25 +1,40 @@
 package br.com.dbc.vemser.cinedev.dto;
 
 import br.com.dbc.vemser.cinedev.entity.enums.Disponibilidade;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class IngressoCreateDTO {
 
+    @NotNull
+    @NotEmpty
+    @Schema(description = "id do cliente ", example = "1")
     private int idCliente;
 
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Valor do ingresso ", example = "30")
     private double preco;
 
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Data e Hora ", example = "31/10/2022 21:30")
     private LocalDateTime dataHora;
 
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Disponibilidade", example = "S ou N")
     private Disponibilidade disponibilidade;
 
 }
