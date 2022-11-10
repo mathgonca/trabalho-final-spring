4c79adc2156757e714789b172843bee37fbaf7a3
Julio Rocha <juliogabrielmr@gmail.com>
2022-11-10 20:59:36 -0300

    att

--- src/main/java/br/com/dbc/vemser/cinedev/repository/GeradorDeSenhas.java
+++ src/main/java/br/com/dbc/vemser/cinedev/repository/GeradorDeSenhas.java
@@ -1,15 +1,20 @@
 package br.com.dbc.vemser.cinedev.repository;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 public class GeradorDeSenhas {
 
     public static void main(String[] args) {
 
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        String senha = bCryptPasswordEncoder.encode("123");
-        System.out.println(senha);
+        SCryptPasswordEncoder sCryptPasswordEncoder = new SCryptPasswordEncoder();
+//        String senha = sCryptPasswordEncoder.encode("123");
+//        System.out.println(senha);
 
+        String senha = "123";
+
+        String senha2 = "$e0801$drP1lh89Cky2j7lwePorT4GSyXU7gtiPXB2SpSvSnif51/qnoIjPP5rNsdrv9isqqQPzdXu/s9DTmUBWfICExw==$EqABe9u5Vq/SWKF/gHxU6JinBioFAD8zEYLzpqSTe1M=";
+        System.out.println(sCryptPasswordEncoder.matches(senha, senha2));
 
     }
 }
